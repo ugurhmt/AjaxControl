38346df297adb3177a8d43cdbd69a636a6a76c53
ugurhmt <ugurh.1995@gmail.com>
2019-09-09 02:04:04 +0300

    Create AjaxFormSubmit Class

---
+++ AjaxFormSubmit.ts
@@ -0,0 +1,20 @@
+namespace AjaxControl{
+    
+    export class AjaxFormSubmit {
+        /*
+        submitForm( formName : string, successCallback : Function, globfunction : any, onsubmitFunction? : Function ){
+            
+            if(onsubmitFunction != null) onsubmitFunction();
+
+            document.querySelectorAll( "form[name = "+ formName +"]" ).onsubmit = () => {
+                
+                let data = new FormData(this);
+                
+
+            }
+
+        }
+        */
+    }
+
+}
